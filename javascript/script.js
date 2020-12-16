a08783c12e4eedad86b03edca864c953cc129b1d
vok2ds <vok00@naver.com>
2020-12-16 12:17:54 +0900

    sample2

--- javascript/script.js
+++ javascript/script.js
@@ -22,14 +22,14 @@ $(".mainmenu>li").mouseleave(function(){
 
 });
 
-// var partnerimg=document.querySelectorAll("partner");
-// var active=document.creatElementByClass("active");
-// var img=document.querySelectorAll("img");
-// var modal=document.querySelector("#modal");
-// var modalbtn=document.querySelector("button");
-// partnerimg.addEventListener("click",function(){
-// 	modal.setAttribute("class","active")
-// });
-// modalbtn.addEventListener("click",function(){
-// 	modal.
-// });
+var partnerimg=document.querySelectorAll("partner");
+var active=document.creatElementByClass("active");
+var img=document.querySelectorAll("img");
+var modal=document.querySelector("#modal");
+var modalbtn=document.querySelector("button");
+partnerimg.addEventListener("click",function(){
+	modal.setAttribute("class","active")
+});
+modalbtn.addEventListener("click",function(){
+	modal.
+});
